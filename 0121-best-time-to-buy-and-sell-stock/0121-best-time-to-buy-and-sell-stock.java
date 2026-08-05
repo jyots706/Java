e2d766731ecde4249aa751d6ef153ce29529313f
Jyotsana Gupta <jyotsanagupta211@gmail.com>
2026-08-05 22:41:42 +0530

    Time: 2 ms (41.97%), Space: 94.4 MB (52.06%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0];
+        int max_profit=0;
+        for(int i=1;i<prices.length;i++){
+            max_profit=Math.max(max_profit,prices[i]-min);
+            min=Math.min(min,prices[i]);
+        }
+        return max_profit;
+    }
+}
